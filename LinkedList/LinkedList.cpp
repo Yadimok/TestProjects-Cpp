145db4101dde9d1b8ce4c6b65e2b229527c943a2
Yadimok <dimokworker@gmail.com>
2015-07-09 23:08:18 +0300

    Add methods

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+template <typename T>
+class LinkedList
+{
+	template <typename U>
+	struct Node
+	{
+		Node (U value)
+		{
+			value_ = value;
+			next_ = nullptr;
+		}
+
+		U		value_;
+		Node<U> *next_;
+	};
+
+	Node<T>	*root_;
+	size_t	size_;
+
+public:
+	LinkedList();
+	~LinkedList();
+
+	bool isEmpty() const;
+	size_t getSize() const;
+	bool find(T item);
+
+	void display();
+	size_t count(T value);
+	T getNth(size_t index);
+
+};
+
+template <typename T>
+LinkedList<T>::LinkedList()
+{
+	root_ = nullptr;
+	size_ = 0;
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList()
+{
+	size_ = 0;
+}
+
+template <typename T>
+bool LinkedList<T>::isEmpty() const
+{
+	return (root_ == nullptr) ? true : false;
+}
+
+template <typename T>
+size_t LinkedList<T>::getSize() const
+{
+	return size_;
+}
+
+template <typename T>
+bool LinkedList<T>::find(T item)
+{
+	Node<T> *tmp = root_;
+	while (tmp != nullptr)
+	{
+		if (tmp->next_ == item)
+			return true;
+		tmp = tmp->next_;
+	}
+	return false;
+
+}
+
+template <typename T>
+void LinkedList<T>::display()
+{
+	Node<T> *tmp = root_;
+	while (tmp != nullptr)
+	{
+		std::cout << tmp->value_ << ", ";
+		tmp = tmp->next_;
+	}
+	std::cout << std::endl;
+}
+
+template <typename T>
+size_t LinkedList<T>::count(T value)
+{
+	size_t countValue = 0;
+	Node<T> *tmp = root_;
+
+	while (tmp != nullptr)
+	{
+		if (tmp->value_ == value)
+			countValue;
+		tmp = tmp->next_;
+	}
+	return countValue;
+
+}
+
+template <typename T>
+T LinkedList<T>::getNth(size_t index)
+{
+	size_t countValue = 0;
+	Node<T> *tmp = root_;
+
+	while (tmp != nullptr)
+	{
+		if (countValue == index)
+			return tmp->value_;
+		countValue++;
+		tmp = tmp->next_;
+	}
+}
+
+
+
+int main()
+{
+
+}
