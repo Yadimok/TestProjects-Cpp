55d01026acb95c90b054bde50d55a8a20e1774a9
Yadimok <dimokworker@gmail.com>
2015-07-22 23:35:37 +0300

    Add struct

---
+++ SkipList/SkipList.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename T, int maxLevel>
+class SkipList
+{
+
+	template <typename U, int level>
+	struct SkipNode
+	{
+		SkipNode()
+
+		U 
+	};
+
+
+
+public:
+	SkipList();
+	~SkipList();
+	
+};
+
+int main()
+{
+
+}
