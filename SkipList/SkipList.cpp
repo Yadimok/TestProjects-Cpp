13939f35f2e0a3f160682ae5c0966967c4be5799
Yadimok <dimokworker@gmail.com>
2015-07-24 13:52:55 +0300

    Add random

--- SkipList/SkipList.cpp
+++ SkipList/SkipList.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
+#include <cstdlib>
 
-template <typename T, int maxLevel>
+template <typename K, typename V, int maxLevel=8>
 class SkipList
 {
 
-	template <typename U, int level>
+	template <typename T, typename U, int MAX>
 	struct SkipNode
 	{
-		SkipNode()
+		SkipNode(T key, U value) : key_(key), value_(value)
+		{
+			for (int i=1; i<MAX; ++i)
+				next_[i] = nullptr;
+		}
 
-		U 
+		T	key_;
+		U	value_;
+
+		SkipNode<T, U, MAX>	*next_[MAX+1];
 	};
 
+	SkipNode<K, V, maxLevel>	*list_;
 
+
+	// random level
+	int randomLevel()
+	{
+		int level = 1;
+		float p = 0.5;
+
+	}
 
 public:
 	SkipList();
