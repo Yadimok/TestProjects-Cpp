ab229177f8aaa747840cc6cc37ab65ea75ae665a
Yadimok <dimokworker@gmail.com>
2015-07-25 16:09:59 +0300

    Fix

--- SkipList/SkipList.cpp
+++ SkipList/SkipList.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <random>
 
-template <typename K, typename V, int maxLevel=8>
+const int MAX_LEVEL = 8;
+
+template <typename K, typename V>
 class SkipList
 {
 
-	template <typename T, typename U, int MAX>
+	template <typename T, typename U>
 	struct SkipNode
 	{
 		SkipNode(T key, U value) : key_(key), value_(value)
 		{
-			for (int i=1; i<MAX; ++i)
+			for (int i=1; i<MAX_LEVEL; ++i)
 				next_[i] = nullptr;
 		}
 
 		T	key_;
 		U	value_;
 
-		SkipNode<T, U, MAX>	*next_[MAX+1];
+		SkipNode<T, U>	*next_[MAX_LEVEL+1];
 	};
-
-	SkipNode<K, V, maxLevel>	*list_;
 
 	int randomLevel()
 	{
@@ -31,27 +31,43 @@ class SkipList
 		std::mt19937 rdg(rd());
 		std::uniform_int_distribution<int> uni(0, 1);
 
-		while (uni(rdg) < p && level < maxLevel)
+		while (uni(rdg) < p && level < MAX_LEVEL)
 			level;
 
 		return level;
 	}
 
+	SkipNode<K, V>	*skiplist_;
+	K 				minKey;
+	K 				maxKey;
+	int 			currentLevel;
+
 
 public:
-	SkipList() {}
-	~SkipList() {}
+	SkipList();
+	~SkipList();
 
-		// random level
-
-	
 };
+
+template <typename K, typename V>
+SkipList<K, V>::SkipList()
+{
+
+}
+
+template <typename K, typename V>
+SkipList<K, V>::~SkipList()
+{
+
+}
 
 int main()
 {	
 	SkipList<int, char> sl;
 	std::cout << std::endl;
-	std::cout << sl.randomLevel() << std::endl;
+	// std::cout << sl.randomLevel() << std::endl;
 	std::cout << std::endl;
+
+
 
 }
