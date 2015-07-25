770121926b411b6d295c0c1d304eb1a81bf8405a
Yadimok <dimokworker@gmail.com>
2015-07-26 00:10:54 +0300

    Issues with UB after delete uncontains value

--- SkipList/SkipList.cpp
+++ SkipList/SkipList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 // #include <random>
 
 const int MAX_LEVEL = 5;
@@ -14,8 +15,7 @@ class SkipList
 		SkipNode(U value, int level) : value_(value), level_(level)
 		{
 			next_ = new SkipNode<U>*[level_+1];
-			for (int i=0; i<level_+1; i++)
-				next_[i] = nullptr;
+			memset(next_, 0, sizeof(SkipNode<U> *) * (level_ + 1));
 		}
 
 		~SkipNode()
@@ -29,12 +29,10 @@ class SkipList
 		SkipNode<U>	**next_;
 	};
 
-
-
 	int randomLevel()
 	{
-		int level = 1;
-		double p = 0.5;
+		int level 	= 1;
+		double p 	= 0.5;
 		while ((std::rand()/static_cast<double>(RAND_MAX)) < p && level < MAX_LEVEL)
 			level++;
 		return level;
@@ -47,7 +45,7 @@ class SkipList
 	}
 
 
-	SkipNode<V>		*skiplistHeader_;
+	SkipNode<V>		*skipListRoot_;
 	int 			currentLevel;
 
 
@@ -58,28 +56,29 @@ public:
 	bool search(V value);
 	void display();
 	void insert(V value);
-
+	void remove(V value);
 };
 
 template <typename V>
 SkipList<V>::SkipList()
 {
-	skiplistHeader_		= new SkipNode<V>(V(), MAX_LEVEL);
+	skipListRoot_		= new SkipNode<V>(V(), MAX_LEVEL);
 	currentLevel 		= 0;
 }
 
 template <typename V>
 SkipList<V>::~SkipList()
 {
-	delete skiplistHeader_;
-	skiplistHeader_ = nullptr;
+	delete skipListRoot_;
+	skipListRoot_ = nullptr;
 }
 
 template <typename V>
 void SkipList<V>::insert(V value)
 {
-	SkipNode<V> *tmpNode = skiplistHeader_;
+	SkipNode<V> *tmpNode = skipListRoot_;
 	SkipNode<V> *update[MAX_LEVEL+1];
+	memset(update, 0, sizeof(SkipNode<V> *) * (MAX_LEVEL + 1));
 
 	for (int level=currentLevel; level >= 0; level--)
 	{
@@ -94,11 +93,10 @@ void SkipList<V>::insert(V value)
 	if (tmpNode == nullptr || tmpNode->value_ != value)
 	{
 		int newLevel = randomLevel();
-		std::cout << newLevel << std::endl;
 
 		if (newLevel > currentLevel) {
 			for (int level = currentLevel+1; level <= newLevel; level++)
-				update[level] = skiplistHeader_;
+				update[level] = skipListRoot_;
 			currentLevel = newLevel;	
 		}
 
@@ -114,7 +112,7 @@ void SkipList<V>::insert(V value)
 template <typename V>
 void SkipList<V>::display()
 {
-	const SkipNode<V> *tmpNode = skiplistHeader_->next_[0];
+	const SkipNode<V> *tmpNode = skipListRoot_->next_[0];
 
 	std::cout << ":-> ";
 	while (tmpNode != nullptr)
@@ -128,15 +126,59 @@ void SkipList<V>::display()
 template <typename V>
 bool SkipList<V>::search(V value)
 {
-	SkipNode<V> *tmpNode = skiplistHeader_;
+	SkipNode<V> *tmpNode = skipListRoot_;
 	for (int i=currentLevel; i>=0; i--)
 	{
-		while (tmpNode->next_[i]->value_ < value)
+		while (tmpNode->next_[i] != nullptr && 	tmpNode->next_[i]->value_ < value)
 			tmpNode = tmpNode->next_[i];
 	}
 
 	tmpNode = tmpNode->next_[0];
-	return (tmpNode->value_ == value) ? true : false;
+	return (tmpNode != nullptr && tmpNode->value_ == value) ? true : false;
+}
+
+template <typename V>
+void SkipList<V>::remove(V value)
+{
+	SkipNode<V> *tmpNode = skipListRoot_;
+	SkipNode<V> *update[MAX_LEVEL+1];
+	memset(update, 0, sizeof(SkipNode<V> *) * (MAX_LEVEL + 1));
+
+
+	for (int i=currentLevel; i >= 0; i--)
+	{
+		while (tmpNode->next_[i] != nullptr && tmpNode->next_[i]->value_ < value)
+			tmpNode = tmpNode->next_[i];
+		update[i] = tmpNode;
+	}
+
+	std::cout << "dump" << std::endl;
+
+	tmpNode = tmpNode->next_[0];
+
+	if (tmpNode->value_ == value)
+	{
+		for (int i=0; i<=currentLevel; i++)
+		{
+			if (update[i]->next_[i] != tmpNode)
+				break;
+			update[i]->next_[i] = tmpNode->next_[i];
+		}
+
+		std::cout << "dump 2" << std::endl;
+
+		if (search(value))
+			delete tmpNode;
+
+		while (currentLevel > 0 && skipListRoot_->next_[currentLevel] == nullptr)
+			currentLevel--;
+
+			std::cout << 1 << std::endl;
+	}
+	else
+	{
+		std::cout << "Value not found" << std::endl;
+	}
 }
 
 int main()
@@ -153,5 +195,15 @@ int main()
 	slist.display();
 
 	std::cout << (slist.search('B') ? "TRUE" : "FALSE") << std::endl;
+
+	slist.remove('A');
+	slist.display();
+
+	slist.remove('B');
+	slist.display();
+
+	if (slist.search('Z'))
+		slist.remove('Z');
+	slist.display();
 
 }
